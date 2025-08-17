c6c6240c7dd8039782602a634c39047e5c1eb98b
unknown <thecguy73@gmail.com>
2025-08-17 12:18:29 +0200

    adding theme source code

---
+++ elisp/royal-sunset-colors.el
@@ -0,0 +1,12 @@
+
+;;; royal-sunset-colors.el --- Color palette for Royal Sunset theme
+
+(defvar royal-sunset-purple "#320D6D" "A deep purple color.")
+(defvar royal-sunset-white "#FFFFFF" "A bright white color.")
+(defvar royal-sunset-yellow "#FFD447" "A vibrant yellow color.")
+(defvar royal-sunset-magenta "#700353" "A rich magenta color.")
+(defvar royal-sunset-brown "#4C1C00" "A dark brown color.")
+
+(provide 'royal-sunset-colors)
+
+;;; royal-sunset-colors.el ends here
---
+++ themes/royal-sunset-theme.el
@@ -0,0 +1,29 @@
+;;; royal-sunset-theme.el --- A modern, dark theme with a sunset vibe.
+
+(require 'royal-sunset-colors)
+
+;;;###autoload
+(deftheme royal-sunset "A theme with a royal sunset color palette.")
+
+(let ((class '((class color) (min-colors 89))))
+  (custom-theme-set-faces
+   'royal-sunset
+   `(default ((,class (:background ,royal-sunset-brown :foreground ,royal-sunset-white))))
+   `(cursor ((,class (:background ,royal-sunset-yellow))))
+   `(region ((,class (:background ,royal-sunset-purple :foreground ,royal-sunset-white))))
+   `(font-lock-keyword-face ((,class (:foreground ,royal-sunset-yellow))))
+   `(font-lock-string-face ((,class (:foreground ,royal-sunset-yellow))))
+   `(font-lock-comment-face ((,class (:foreground ,royal-sunset-purple))))
+   `(font-lock-function-name-face ((,class (:foreground ,royal-sunset-magenta))))
+   `(font-lock-variable-name-face ((,class (:foreground ,royal-sunset-white))))))
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path)
+           (not (member (file-name-directory (or load-file-name (buffer-file-name)))
+                        custom-theme-load-path)))
+  (add-to-list 'custom-theme-load-path
+               (file-name-directory (or load-file-name (buffer-file-name)))))
+
+(provide-theme 'royal-sunset)
+
+;;; royal-sunset-theme.el ends here
